1af862ecf14df55b2c71c0e0d816cffbb2fb3ac7
sulaimankhan8 <suleman111111111111111@gmail.com>
2026-03-16 22:05:08 +0530

    62 removing logs

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,8 @@ import app from "./app.js";
 import { initSocket } from "./socket.js";
 import { ENV } from "./utils/env.js";
 import { initMediasoup } from "./voice/mediasoup.js";
-import { monitorEventLoopDelay } from "perf_hooks";
 
 const server = http.createServer(app);
-
-/* -------- EVENT LOOP MONITOR -------- */
-
-const h = monitorEventLoopDelay();
-h.enable();
-
-setInterval(() => {
-  console.log("event loop lag ms:", (h.mean / 1e6).toFixed(2));
-}, 2000);
-
-/* --------- STARTUP SEQUENCE --------- */
 
 await initMediasoup();
 initSocket(server);
